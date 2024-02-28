2223f712159be00f0de16a633a6fa2ae44a15f28
Hortensi Martin Florit <martinflorithortensi@gmail.com>
2024-02-28 19:01:44 +0100

    Afegit register de l'usuari

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+from register import *
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return '¡Hola, mundo! Este es un servidor web sencillo con Flask en Python.'
+@app.route('/api/register/', methods=['POST'])
+def register():
+    print("Començar register de l'usuari")
+    email = request.form.get('email')
+    if not email:
+        return jsonify({'error': 'El paràmetre "email" és obligatori.'}), 400
+    registerUser(email)
+
+    return jsonify({"message": f"Usuari registrat amb l'email '{email}'."}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ register.py
@@ -0,0 +1,24 @@
+import argparse
+import json
+import secrets
+
+longitud_bytes = 32
+
+def generar_token(longitud):
+    token_bytes = secrets.token_bytes(longitud_bytes)
+    return secrets.token_hex(longitud_bytes)
+
+def registerUser(email:str):
+    try:
+        with open('db.json', 'r') as json_file:
+            data = json.load(json_file)
+    except FileNotFoundError:
+        data = {"users": []}
+    
+    token = generar_token(longitud_bytes)
+    user = {"correo_electronico": email, "token": token}
+    data["users"].append(user)
+    with open('db.json', 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
+    print(f"Se ha registrado el usuario con el correo electrónico '{email}' y el token '{token}'.")
